959e97306d433e918daab17c2f18e2f8382d4386
Thibaut Sabot <thibaut.sabot@adevinta.com>
2021-03-17 21:09:31 +0100

    add second action

--- src/actions/secondaction.ts
+++ src/actions/secondaction.ts
@@ -2,29 +2,20 @@ import {
   KeyUpEvent,
   SDOnActionEvent,
   StreamDeckAction,
-  WillAppearEvent,
 } from 'streamdeck-typescript'
 
 import { Boilerplate } from '../boilerplate-plugin'
 import { SettingsInterface } from '../boilerplate-property-inspector'
 
-export class BoilerplateAction extends StreamDeckAction<Boilerplate, BoilerplateAction> {
+export class SecondAction extends StreamDeckAction<Boilerplate, SecondAction> {
   constructor(private plugin: Boilerplate, private actionName: string) {
     super(plugin, actionName)
     console.log('Boilerplate constructor')
   }
 
-  @SDOnActionEvent('willAppear')
-  private onAppear({ context, payload: { settings } }: WillAppearEvent<SettingsInterface>) {
-    console.log('onAppear')
-  }
-
   @SDOnActionEvent('keyUp')
   public onKeyUp({ action, context, device }: KeyUpEvent<SettingsInterface>): number {
-    console.log('keyUp')
-    console.log('action : ', action)
-    console.log('context : ', context)
-    console.log('device : ', device)
+    console.log('SecondAction keyUp')
 
     return 42
   }
--- src/boilerplate-plugin.ts
+++ src/boilerplate-plugin.ts
@@ -1,12 +1,14 @@
 import { DidReceiveSettingsEvent, SDOnActionEvent, StreamDeckPluginHandler } from 'streamdeck-typescript'
 
 import { BoilerplateAction } from './actions/action'
+import { SecondAction } from './actions/secondaction'
 import { SettingsInterface } from './boilerplate-property-inspector'
 
 export class Boilerplate extends StreamDeckPluginHandler {
   constructor() {
     super()
     new BoilerplateAction(this, 'com.thibautsabot.streamdeck.boilerplate')
+    new SecondAction(this, 'com.thibautsabot.streamdeck.secondaction')
   }
 
   @SDOnActionEvent('didReceiveGlobalSettings')
